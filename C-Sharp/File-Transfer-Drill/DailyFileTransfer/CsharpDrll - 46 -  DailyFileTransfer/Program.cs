7cbe444ba91c71df25fb235de793a0e9098d9760
TawfikW <wasswass70@gmail.com>
2016-03-14 14:40:36 -0700

    Added Daily File Transfer drill to Portfolio

---
+++ C-Sharp/File-Transfer-Drill/DailyFileTransfer/CsharpDrll - 46 -  DailyFileTransfer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDrll___46____DailyFileTransfer
+{
+    class Ftp2
+    {
+        static void Main()
+        {
+            FileTransfer();
+        }
+
+
+        static void FileTransfer()
+        {
+            string fileName = ".txt";
+            string sourcePath = @"C:\Users\Wes\Desktop\Folder A\";
+            string destPath = @"C:\Users\Wes\Desktop\Folder B\";
+            string sourceFile = Path.Combine(sourcePath, fileName);
+            string destFile = Path.Combine(destPath, fileName);
+
+
+            if (Directory.Exists(destPath))
+            {
+                Directory.CreateDirectory(destPath);
+            }
+
+            if (Directory.Exists(sourcePath))
+            {
+
+                string[] files = Directory.GetFiles(sourcePath);
+                foreach (string s in files)
+                { Console.WriteLine(s); }
+
+                // Copy the files more than 24hrs old, overwrites existing files
+                foreach (string s in files)
+                {
+                    DateTime fileDate = File.GetLastWriteTime(s);
+                    bool timeCheck = fileDate > DateTime.Now.AddHours(-24);
+
+                    if (timeCheck == true)
+                    {
+                        fileName = Path.GetFileName(s);
+                        destFile = Path.Combine(destPath, fileName);
+                        File.Copy(s, destFile, true);
+
+                        Console.WriteLine("Transferring file ... ");
+                    }
+                }
+                Console.WriteLine("\nFile transfer complete.\n");
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist.");
+            }
+
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+
+        }
+
+
+    }
+}
+
--- C-Sharp/File-Transfer-Drill/README.md
+++ C-Sharp/File-Transfer-Drill/README.md
@@ -1,7 +1,7 @@
 ### File Transfer Drill
 ***
 
-Write about objective, tools, steps taken and results
+The objective of this drill was to check a folder (folder A) to verify if any of the files in it were modified in the previous 24 hours, if any file was modified a copy of it would be sent to a second folder (folder B)
  
  
 Return to [portfolio](../../../../) 
